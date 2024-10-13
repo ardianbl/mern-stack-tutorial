a8b61538fca8e403e27f569dae4935c69be60a7a
Ardian Bryan Limasarian <ardian-bryan@outlook.com>
2024-10-13 16:18:58 +0800

    3. create a product model
    created models folder, created productModel.js,

---
+++ backend/models/productModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema( {
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true // createdAt, updatedAt
+});
+
+const Product = mongoose.model('Product', productSchema);
+// why "Product", not "products"?
+// because mongoose will convert the naming into a plural form, and lower-capitalised the first letter
+
+export default Product;
